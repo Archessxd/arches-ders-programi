528e21a891cdf239822b9e918218927d95ee65d4
rasit <108332998+Archessxd@users.noreply.github.com>
2022-12-22 20:59:20 +0300

    Ders_programi
    ders_programi_arches

---
+++ ders_programi_uygulamasi/ders_programi_uygulamasi/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace ders_programi_uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pazartesi",
+            "Salı ",
+            "Çarşamba",
+            "Perşembe",
+            "Cuma"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(34, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Programı Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(274, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 244);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Arches ❤️ Léandre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 272);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Arches Ders Programı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ders_programi_uygulamasi/ders_programi_uygulamasi/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders_programi_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        string[] ders = new string[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //array temizleme
+            for (int i = 0;i<ders.Length;i++)
+            {
+                ders[i] = null;
+            }
+
+
+            listBox1.Items.Clear();
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                ders[0] = "robotik kodlama";
+                ders[1] = "robotik kodlama";
+                ders[2] = "robotik kodlama";
+                ders[3] = "robotik kodlama";
+                ders[4] = "Kimya";
+                ders[5] = "Kimya";
+                ders[6] = "Din Kültürü";
+                ders[7] = "Din Kültürü";
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                ders[0] = "Edebiyat";
+                ders[1] = "Edebiyat";
+                ders[2] = "İngilizce";
+                ders[3] = "İngilizce";
+                ders[4] = "Matematik";
+                ders[5] = "Matematik";
+                ders[6] = "Beden Eğitimi";
+                ders[7] = "Beden Eğitimi";
+            }
+            if (comboBox1.SelectedIndex ==2)
+            {
+                ders[0] = "Biyoloji";
+                ders[1] = "Biyoloji";
+                ders[2] = "Rehberlik";
+                ders[3] = "Edebiyat";
+                ders[4] = "Edebiyat";
+                ders[5] = "Tarih";
+                ders[6] = "Tarih";
+                ders[7] = "Fizik";
+                ders[8] = "Fizik";
+            }
+            if (comboBox1.SelectionLength==3)
+            {
+                for (int i = 0;i<ders.Length;i++)
+                {
+                    ders[i] = "Nesne Tabanlı Programlama";
+                }
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                ders[0] = "Coğrafya";
+                ders[1] = "Coğrafya";
+                ders[2] = "Felsefe";
+                ders[2] = "Felsefe";
+                ders[3] = "Edebiyat";
+                ders[4] = "Matematik";
+                ders[4] = "Matematik";
+                ders[4] = "Matematik";
+            }
+
+
+            for (int i = 0;i<ders.Length;i++)
+            {
+                if (ders[i] == null) 
+                {
+                    continue;
+                }
+
+                listBox1.Items.Add(ders[i].ToString());
+            }
+        
+        
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
